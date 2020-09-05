1ae7d719f4dd244538054b968724442a45b3288c
Gabriel Miranda <mirandacristiangabriel56@gmail.com>
2020-09-04 22:11:25 -0300

    Mi primer commit

---
+++ tp1_laboratorio_2_C_Gabriel_Miranda/tp1_laboratorio_2_C_Gabriel_Miranda/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp1_laboratorio_2_C_Gabriel_Miranda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!!");
+            Console.ReadKey();
+        }
+    }
+}
